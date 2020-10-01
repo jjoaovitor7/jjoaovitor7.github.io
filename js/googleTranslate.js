beff45ec81a4edc0d60dc2a47b979df69b94c1b4
João Vítor <joaovitorsf7@hotmail.com>
2020-10-01 13:08:25 -0300

    google translate mobile.

--- js/googleTranslate.js
+++ js/googleTranslate.js
@@ -1,3 +1,10 @@
 function googleTranslateElementInit(){
-    new google.translate.TranslateElement({pageLanguage: 'pt_BR', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google-translate');
+    if (window.innerWidth <= 768){
+        new google.translate.TranslateElement({pageLanguage: 'pt_BR'}, 'google-translate');
+
+    }
+    else {
+        new google.translate.TranslateElement({pageLanguage: 'pt_BR', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google-translate');
+    }
 }
+
