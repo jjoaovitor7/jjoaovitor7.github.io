ea69d2f613539818f6141133f26fc8e0c11b3fcf
João Vítor <joaovitorsf7@hotmail.com>
2021-01-09 11:06:41 -0300

    fix.

---
+++ public/scripts/mobileNav.js
@@ -0,0 +1,9 @@
+function mobileNav() {
+  const mnu = document.getElementById("mobile-nav-urls");
+
+  if (mnu.style.display == "block") {
+    mnu.style.display = "none";
+  } else {
+    mnu.style.display = "block";
+  }
+}
