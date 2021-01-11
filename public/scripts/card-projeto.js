70b1ca36f60e5e860d79d880c7a46106edaa5b8e
João Vítor <joaovitorsf7@hotmail.com>
2021-01-11 20:50:07 -0300

    mudança de cor do card.

--- public/scripts/card-projeto.js
+++ public/scripts/card-projeto.js
@@ -29,7 +29,7 @@ class CardProject extends HTMLElement {
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        background-color: #f1f1f1 !important;
+        background-color: #e8eaf6;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
         width: 175px;
