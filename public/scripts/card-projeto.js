98d8bd4f4d2dc4a361698fa3c6d0cccf9ef5c236
João Vítor <joaovitorsf7@hotmail.com>
2021-01-11 21:11:52 -0300

    fix.

--- public/scripts/card-projeto.js
+++ public/scripts/card-projeto.js
@@ -29,7 +29,6 @@ class CardProject extends HTMLElement {
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        background-color: #f1f1f1 !important;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         transition: 0.3s;
         width: 175px;
