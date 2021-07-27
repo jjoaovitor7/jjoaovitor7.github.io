a1e636c0512e430dee136d73ea98a0641749e60c
João Vítor <joaovitorsf7@hotmail.com>
2021-07-27 15:20:46 -0300

    big update. refatoração. mbd removido, bootstrap 5 adicionado, animate.css removido. páginas de projetos e ferramentas, redirecionamentos e vários scripts excluídos. menu desktop e menu mobile removidos. alguns sites removidos do sitemap.

--- public/scripts/date.js
+++
@@ -1,1 +0,0 @@
-document.write("<p>Data atual: " + Date() + "</p>");
--- public/scripts/mobileNav.js
+++
@@ -1,9 +0,0 @@
-function mobileNav() {
-  const mnu = document.getElementById("mobile-nav-urls");
-
-  if (mnu.style.display == "block") {
-    mnu.style.display = "none";
-  } else {
-    mnu.style.display = "block";
-  }
-}
