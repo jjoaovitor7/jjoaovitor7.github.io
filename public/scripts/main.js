f69694be9717e22fa026e7c6d0e9a7cf600ebbeb
João Vítor <joaovitorsf7@hotmail.com>
2022-07-08 12:41:34 -0300

    refatoração.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,67 +1,35 @@
-window.onload = function () {
-  const containerToast = document.querySelector(".container__loader");
-  containerToast.style.opacity = "0";
-  containerToast.addEventListener("transitionend", () => {
-    containerToast.remove();
-  });
-};
-
 class CardProject extends HTMLElement {
   constructor() {
     super();
 
-    const name = this.getAttribute("name");
-    const url = this.getAttribute("url");
-    const langs = this.getAttribute("langs");
-
     const shadow = this.attachShadow({ mode: "open" });
-
     const wrapper = document.createElement("div");
     wrapper.classList = "card";
-
     wrapper.innerHTML = `
         <p>
-          <a
-            target="_blank"
-            href=${url}
-          >
-          <b>${name}</b>
+          <a target="_blank" href=${this.getAttribute("url")}>
+            <strong>${this.getAttribute("name")}</strong>
           </a>
         </p>
-        <p>${langs}</p>`;
+        <p>${this.getAttribute("langs")}</p>`;
 
-    const style = document.createElement("style");
-    style.textContent = `
-        .card {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          box-shadow: 0 .4rem .8rem 0 rgba(0, 0, 0, 0.2);
-          transition: 0.3s;
-          width: 17.5rem;
-          height: 12rem;
-          text-align: center;
-          margin: .5rem;
-        }
-  
-        .card:hover {
-          box-shadow: 0 .8rem 1.6rem 0 rgba(0, 0, 0, 0.2);
-        }
-  
-        .card p {
-          margin: .5rem;
-          font-family: "Lato", sans-serif;
-          font-size: 1.6rem;
-        }
-  
-        .card a {
-          text-decoration: none;
-        }
-        `;
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", "./public/styles/card-project.css");
 
     shadow.append(style, wrapper);
   }
 }
-
 customElements.define("card-project", CardProject);
+
+window.onload = function() {
+  const container_loader = document.getElementById("container-loader");
+  container_loader.style.opacity = "0";
+  container_loader.addEventListener("transitionend", () => {
+    container_loader.remove();
+  });
+
+  let today = new Date();
+  let date = `${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}`;
+  document.getElementById("date").innerHTML = `<p>Data atual: <br />${date}</p>`;
+};
