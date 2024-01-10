5089be05ebf6fbea2e71421a267a417cb5dde07c
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-01-09 23:57:24 -0300

    ajustes, refatoração e substituindo o vanta particles.js por problemas de desempenho.

---
+++ public/scripts/card.project.js
@@ -0,0 +1,23 @@
+class CardProject extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    const wrapper = document.createElement("div");
+    wrapper.classList = "card block my-4";
+    wrapper.innerHTML = `
+        <p>
+          <a target="_blank" href=${this.getAttribute("url")}>
+            <strong>${this.getAttribute("name")}</strong>
+          </a>
+        </p>
+        <p>${this.getAttribute("stack")}</p>`;
+
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("href", "./public/styles/card-project.css");
+
+    shadow.append(style, wrapper);
+  }
+}
+customElements.define("card-project", CardProject);
