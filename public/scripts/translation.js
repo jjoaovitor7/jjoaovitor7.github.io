e18d268f1ec2a39cd9cb31a3b71b7dd6b514e70a
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-09-19 19:17:43 -0300

    feat: +tradução, melhorias nos textos e ajuste na opacidade do particlejs.

---
+++ public/scripts/translation.js
@@ -0,0 +1,181 @@
+const translations = {
+  "pt-br": {
+    "index": "Início",
+    "education": "Educação",
+    "work": "Experiência Profissional",
+    "projects": "Projetos",
+    "presentation": "Apresentação",
+    "about": [
+      "Olá, meu nome é João Vítor, sou formado em Ciência da Computação e tenho mais de 3 anos de experiência \
+        no setor de TI, com foco no desenvolvimento back-end e front-end (web & mobile).",
+      "Estou trabalhando como desenvolvedor full-stack na Empresa Sergipana de Tecnologia de Informação (EMGETIS), onde \
+        desenvolvo novas funcionalidades para sites de órgãos governamentais e responsável pelo desenvolvimento do app \"Gol \
+        da Gente\" (API, Android, iOS e Web), cujo objetivo é trocar NFe's por ingressos de eventos esportivos.",
+      "Ah, também tenho familiaridade com Kanban e posso auxiliar em traduções textuais de Inglês (en) para Português Brasileiro (pt-BR).",
+    ],
+    "roles": [
+      "Desenvolvedor Full-Stack",
+      "Desenvolvedor Web",
+      "Desenvolvedor Mobile",
+      "Tradutor"
+    ],
+    "education_1": {
+      "title": "Bacharelado em Ciência da Computação pela Universidade Tiradentes - UNIT.",
+      "period": "jan. 2018 - ago. 2023",
+    },
+    "work_1": {
+      "title": "Desenvolvedor Full-Stack, EMGETIS.",
+      "period": "ago. 2021 - até o momento",
+      "info": [
+        "(alocado na ARSIN)",
+        "Desenvolvimento e manutenção (com melhorias de segurança) de mais de 32 sites \
+          com o CMS WordPress e a stack <abbr title=\"Linux, Apache, MariaDB e PHP\">LAMP</abbr>.",
+        "Implementação do Composer \
+          para gerenciamento de dependências de temas de sites WordPress, \
+          o integrando com o Bower e o NPM.",
+        "Criação de shortcodes e blocos customizados, \
+          além da implementação e configuração de alguns plugins (ACF, Ninja Forms, entre outros) para sites WordPress \
+          buscando melhorar a experiência do usuário, \
+          e realização de alguns webscraping.",
+        "Implementação do Git com Gitea para versionamento do código \
+          e criação de script CI/CD com Gitea Actions para envio de alterações relacionadas ao tema WordPress de \
+          ambiente de \"dev.\" pra ambiente de \"prod.\".",
+        "Desenvolvimento e disponibilização em lojas virtuais (Google Play Store e Apple App Store) de aplicação mobile com React Native. \
+          Desenvolvimento da API com JS/TS, aplicando conceitos como MVC, patterns como Middleware e Repository, entre outros. \
+          Integração com a API da Secretaria de Estado da Fazenda (SEFAZ-SE), do gov.br, do IBGE e com o webservice do ViaCEP (json). \
+          Port para versão Web.",
+        "Utilização do Portainer para hospedagem de sites e aplicações em desenvolvimento a partir de \"Stacks\" \
+          <i>(docker compose)</i>.",
+        "Customização do GLPI <i>(visual e notificações por e-mail).</i>"
+      ],
+    },
+    "work_2": {
+      "title": "Estagiário de TI, EMGETIS.",
+      "period": "dez. 2019 - ago. 2021",
+      "info": [
+        "Atividades Desempenhadas:",
+        "Desenvolvimento e manutenção de alguns sites com o CMS WordPress e a stack \
+          <abbr title=\"Linux, Apache, MariaDB e PHP\">LAMP</abbr>.",
+        "Atendimento de chamados pelo GLPI <i>(helpdesk)</i>, \
+          além do suporte ao usuário em sistemas do Detran <i>(emulador)</i>, \
+          Expresso Livre <i>(e-mail)</i> \
+          e Zimbra <i>(e-mail)</i>.",
+        "Visão Geral do Zabbix <i>(monitoramento de infraestrutura e rede)</i> e do Ansible.",
+        "Backup de base de dados <abbr title=\"Microsoft SQL Server\">mssql</abbr> utilizando o RDesktop <i>(para \
+          acesso remoto a máquina)</i> e o <abbr title=\"Server Management Studio\">SMSS</abbr>."
+      ]
+    }
+  },
+  "en": {
+    "index": "Home",
+    "education": "Education",
+    "work": "Work Experience",
+    "projects": "Projects",
+    "presentation": "About",
+    "about": [
+      "Hi, my name is \"João Vítor\", i'm graduated in Computer Science and i have more than three years of experience \
+        in IT sector, mainly in back-end and front-end development (web & mobile).",
+      "I'm working as full-stack developer at EMGETIS, where I \
+        develop new features for public authority websites and e responsible for the development of the app \"Gol \
+        da Gente\" (API, Android, iOS e Web), whose objective is to exchange NF-e's for sporting events tickets.",
+      "Oh, i'm also familiar with Kanban and can help with textual translations from English (en) to Brazilian Portuguese (pt-BR).",
+    ],
+    "roles": [
+      "Full-Stack Developer",
+      "Web Developer",
+      "Mobile Developer",
+      "Translator"
+    ],
+    "education_1": {
+      "title": "Bachelor's degree in Computer Science from Tiradentes University - UNIT",
+      "period": "jan. 2018 - aug. 2023",
+    },
+    "work_1": {
+      "title": "Full-Stack Developer, EMGETIS.",
+      "period": "aug. 2021 - until now",
+      "info": [
+        "(allocated at ARSIN)",
+        "Development and maintenance (with security improvements) of more than 32 websites \
+          with the WordPress CMS and the <abbr title=\"Linux, Apache, MariaDB and PHP\">LAMP</abbr>.",
+        "Composer implementation \
+          for WordPress website theme dependency management, \
+          integrating it with Bower and NPM.",
+        "Creation of shortcodes and custom blocks,\
+          in addition to implementation and configuration of some plugins (ACF, Ninja Forms, among others) for WordPress websites \
+          focus to improve the user experience (UX), \
+          and doing some webscraping.",
+        "Git implementation with Gitea for code versioning\
+          and creating CI/CD script with Gitea Actions to send changes related to the WordPress theme from \
+          \"dev.\" environment to \"prod.\" environment.",
+        "Development and availability in virtual stores (Google Play Store and Apple App Store) of a mobile application with React Native.\
+          API development with JS/TS, applying concepts such as MVC, patterns as Middleware and Repository, among others. \
+          Integration with the API of SEFAZ-SE, gov.br, IBGE and the ViaCEP webservice (json). \
+          Port for Web version.",
+        "Using Portainer to host websites and applications under development from \"Stacks\" \
+          <i>(docker compose)</i>.",
+        "GLPI customization <i>(visual and email notifications).</i>"
+      ],
+    },
+    "work_2": {
+      "title": "IT Intern, EMGETIS.",
+      "period": "dec. 2019 - aug. 2021",
+      "info": [
+        "Activities Performed:",
+        "Development and maintenance of some websites with the WordPress CMS and the \
+          <abbr title=\"Linux, Apache, MariaDB and PHP\">LAMP</abbr> stack.",
+        "Request Fulfillment using GLPI <i>(helpdesk software)</i>. \
+          in addition to user support on Detran systems <i>(emulator)</i>, \
+          Expresso Livre <i>(e-mail)</i> \
+          and Zimbra <i>(e-mail)</i>.",
+        "Overview of Zabbix <i>(infrastructure and network monitoring)</i> and Ansible.",
+        "Backup database <abbr title=\"Microsoft SQL Server\">mssql</abbr> using RDesktop <i>(for \
+          remote access to the machine)</i> and <abbr title=\"Server Management Studio\">SMSS</abbr>."
+      ]
+    }
+  }
+}
+
+let userLang = (navigator.language || navigator.userLanguage).toLowerCase();
+if (!["en", "pt-br"].includes(userLang)) {
+  // fallback
+  userLang = "en";
+}
+document.documentElement.lang = userLang;
+
+function NativeTemplate() {
+  const htmlContent = document.body.innerHTML;
+  const fmtContent = htmlContent.replace(/\{\{(.*?)\}\}/g, (_, key) => {
+    key = key.trim();
+    const valueArr = key.split('.');
+    let arrOption;
+
+    const getOption = (str) => str.match(/\[([^\]]+)\]/);
+    const hasOptions = (arr) => ![null, undefined].includes(arr);
+    const clearOption = (str) => str.replace(/\[.*?\]/g, '');
+
+    if (valueArr.length == 2) {
+      arrOption = getOption(valueArr[1]);
+      if (hasOptions(arrOption)) {
+        valueArr[1] = clearOption(valueArr[1])
+        return translations[userLang][valueArr[1]][parseInt(arrOption[1])];
+      }
+    }
+
+    if (valueArr.length == 3) {
+      arrOption = getOption(valueArr[2]);
+      if (hasOptions(arrOption)) {
+        valueArr[2] = clearOption(valueArr[2])
+        return translations[userLang][valueArr[1]][valueArr[2]][parseInt(arrOption[1])];
+      }
+    }
+
+    if (valueArr.length <= 2) {
+      return translations[userLang][valueArr[1]];
+    } else {
+      return translations[userLang][valueArr[1]][valueArr[2]];
+    }
+  });
+  document.body.innerHTML = fmtContent;
+}
+
+NativeTemplate();
