f6d1d247ad021a768a404d88a47641793aefe1bb
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2024-10-13 15:49:16 -0300

    refactor: melhorias nos textos.

--- public/scripts/translation.js
+++ public/scripts/translation.js
@@ -11,7 +11,7 @@ const translations = {
       "Estou trabalhando como desenvolvedor full-stack na Empresa Sergipana de Tecnologia de Informação (EMGETIS), onde \
         desenvolvo novas funcionalidades para sites de órgãos governamentais e responsável pelo desenvolvimento do app \"Gol \
         da Gente\" (API, Android, iOS e Web), cujo objetivo é trocar NFe's por ingressos de eventos esportivos.",
-      "Ah, também tenho familiaridade com Kanban e posso auxiliar em traduções textuais de Inglês (en) para Português Brasileiro (pt-BR).",
+      "Também tenho familiaridade com Kanban e posso auxiliar em traduções textuais de Inglês (en) para Português Brasileiro (pt-BR).",
     ],
     "roles": [
       "Desenvolvedor Full-Stack",
@@ -28,11 +28,11 @@ const translations = {
       "period": "ago. 2021 - até o momento",
       "info": [
         "(alocado na ARSIN)",
-        "Desenvolvimento e manutenção (com melhorias de segurança) de mais de 32 sites \
-          com o CMS WordPress e a stack <abbr title=\"Linux, Apache, MariaDB e PHP\">LAMP</abbr>.",
-        "Implementação do Composer \
-          para gerenciamento de dependências de temas de sites WordPress, \
-          o integrando com o Bower e o NPM.",
+        "Desenvolvimento e manutenção (+32 sites) com o CMS WordPress \
+          e a stack <abbr title=\"Linux, Apache2, MariaDB e PHP\">LAMP</abbr>. \
+          Implementação do Composer \
+          para gerenciamento de dependências dos temas desses sites, \
+          o integrando com o Bower e o NPM. Melhorias de segurança.",
         "Criação de shortcodes e blocos customizados, \
           além da implementação e configuração de alguns plugins (ACF, Ninja Forms, entre outros) para sites WordPress \
           buscando melhorar a experiência do usuário, \
@@ -40,11 +40,13 @@ const translations = {
         "Implementação do Git com Gitea para versionamento do código \
           e criação de script CI/CD com Gitea Actions para envio de alterações relacionadas ao tema WordPress de \
           ambiente de \"dev.\" pra ambiente de \"prod.\".",
-        "Desenvolvimento e disponibilização em lojas virtuais (Google Play Store e Apple App Store) de aplicação mobile com React Native. \
-          Desenvolvimento da API com JS/TS, aplicando conceitos como MVC, patterns como Middleware e Repository, entre outros. \
-          Integração com a API da Secretaria de Estado da Fazenda (SEFAZ-SE), do gov.br, do IBGE e com o webservice do ViaCEP (json). \
-          Port para versão Web.",
-        "Customização do GLPI <i>(visual com twig e notificações por e-mail).</i>",
+        "Desenvolvimento com React Native do app mobile \"Gol da Gente\" \
+          e disponibilização em lojas virtuais (Google Play Store e Apple App Store). \
+          Desenvolvimento da API p/ o app com JS/TS, aplicando conceitos como MVC, patterns como Middleware e Repository \
+          e a integrando com a API da SEFAZ-SE, do gov.br, do IBGE e com o webservice do ViaCEP (json). \
+          O SGBD selecionado foi o PostgreSQL principalmente pela sua eficiência \
+          e foi realizado o <i>port</i> do app para versão web.",
+        "Customização visual com twig e das notificações por e-mail do GLPI.",
         "Desenvolvimento de aplicação com Angular, Bootstrap 5, Golang e Docker Compose."
       ],
     },
@@ -52,16 +54,14 @@ const translations = {
       "title": "Estagiário de TI, EMGETIS.",
       "period": "dez. 2019 - ago. 2021",
       "info": [
-        "Atividades Desempenhadas:",
         "Desenvolvimento e manutenção de alguns sites com o CMS WordPress e a stack \
-          <abbr title=\"Linux, Apache, MariaDB e PHP\">LAMP</abbr>.",
-        "Atendimento de chamados pelo GLPI <i>(helpdesk)</i>, \
-          além do suporte ao usuário em sistemas do Detran <i>(emulador)</i>, \
-          Expresso Livre <i>(e-mail)</i> \
-          e Zimbra <i>(e-mail)</i>.",
-        "Visão Geral do Zabbix <i>(monitoramento de infraestrutura e rede)</i> e do Ansible.",
-        "Backup de base de dados <abbr title=\"Microsoft SQL Server\">mssql</abbr> utilizando o RDesktop <i>(para \
-          acesso remoto a máquina)</i> e o <abbr title=\"Server Management Studio\">SMSS</abbr>."
+          <abbr title=\"Linux, Apache2, MariaDB e PHP\">LAMP</abbr>.",
+        "Atendimento de chamados pelo GLPI, \
+          além do suporte ao usuário em sistemas do Detran <i>(emulador)</i> \
+          e dos softwares de gerenciamento de e-mail \"Expresso Livre\" e \"Zimbra\".",
+        "Visão geral do software de monitoramento \"Zabbix\" e do Ansible.",
+        "Backup de base de dados <abbr title=\"Microsoft SQL Server\">mssql</abbr> utilizando o \
+          <abbr title=\"Server Management Studio\">SMSS</abbr> e o RDesktop para acesso remoto a máquina."
       ]
     }
   },
@@ -77,7 +77,7 @@ const translations = {
       "I'm working as full-stack developer at EMGETIS, where I \
         develop new features for public authority websites and e responsible for the development of the app \"Gol \
         da Gente\" (API, Android, iOS e Web), whose objective is to exchange NF-e's for sporting events tickets.",
-      "Oh, i'm also familiar with Kanban and can help with textual translations from English (en) to Brazilian Portuguese (pt-BR).",
+      "I'm also familiar with Kanban and can help with textual translations from English (en) to Brazilian Portuguese (pt-BR).",
     ],
     "roles": [
       "Full-Stack Developer",
@@ -94,11 +94,10 @@ const translations = {
       "period": "aug. 2021 - until now",
       "info": [
         "(allocated at ARSIN)",
-        "Development and maintenance (with security improvements) of more than 32 websites \
-          with the WordPress CMS and the <abbr title=\"Linux, Apache, MariaDB and PHP\">LAMP</abbr>.",
-        "Composer implementation \
-          for WordPress website theme dependency management, \
-          integrating it with Bower and NPM.",
+        "Development and maintenance (+32 sites) with the WordPress CMS \
+          and the <abbr title=\"Linux, Apache2, MariaDB and PHP\">LAMP</abbr>. \
+          Composer implementation for theme dependency management of these sites, \
+          integrating it with Bower and NPM. Security improvements.",
         "Creation of shortcodes and custom blocks,\
           in addition to implementation and configuration of some plugins (ACF, Ninja Forms, among others) for WordPress websites \
           focus to improve the user experience (UX), \
@@ -106,11 +105,13 @@ const translations = {
         "Git implementation with Gitea for code versioning\
           and creating CI/CD script with Gitea Actions to send changes related to the WordPress theme from \
           \"dev.\" environment to \"prod.\" environment.",
-        "Development and availability in virtual stores (Google Play Store and Apple App Store) of a mobile application with React Native.\
-          API development with JS/TS, applying concepts such as MVC, patterns as Middleware and Repository, among others. \
-          Integration with the API of SEFAZ-SE, gov.br, IBGE and the ViaCEP webservice (json). \
-          Port for Web version.",
-        "GLPI customization <i>(visual with twig and email notifications).</i>",
+        "Development with React Native of the mobile app \"Gol da Gente\" \
+          and availability in virtual stores (Google Play Store and Apple App Store).\
+          API development for with JS/TS, applying concepts such as MVC, patterns as Middleware and Repository, \
+          and integrating it with the SEFAZ-SE API, gov.br, IBGE and the ViaCEP webservice (json). \
+          The selected DBMS was PostgreSQL mainly due to its efficiency\
+          and was realized the <i>port</i> of app to the web version.",
+        "Visual customization with twig and email notifications of the GLPI.",
         "Development of application with Angular, Bootstrap 5, Golang and Docker Compose."
       ],
     },
@@ -118,16 +119,14 @@ const translations = {
       "title": "IT Intern, EMGETIS.",
       "period": "dec. 2019 - aug. 2021",
       "info": [
-        "Activities Performed:",
         "Development and maintenance of some websites with the WordPress CMS and the \
-          <abbr title=\"Linux, Apache, MariaDB and PHP\">LAMP</abbr> stack.",
-        "Request Fulfillment using GLPI <i>(helpdesk software)</i>. \
+          <abbr title=\"Linux, Apache2, MariaDB and PHP\">LAMP</abbr> stack.",
+        "Answer requests using GLPI, \
           in addition to user support on Detran systems <i>(emulator)</i>, \
-          Expresso Livre <i>(e-mail)</i> \
-          and Zimbra <i>(e-mail)</i>.",
-        "Overview of Zabbix <i>(infrastructure and network monitoring)</i> and Ansible.",
-        "Backup database <abbr title=\"Microsoft SQL Server\">mssql</abbr> using RDesktop <i>(for \
-          remote access to the machine)</i> and <abbr title=\"Server Management Studio\">SMSS</abbr>."
+          and of e-mail management softwares \"Expresso Livre\" and \"Zimbra\".",
+        "Overview of monitoring software \"Zabbix\" and Ansible.",
+        "Backup database <abbr title=\"Microsoft SQL Server\">mssql</abbr> using \
+        <abbr title=\"Server Management Studio\">SMSS</abbr> and RDesktop for remote access to the machine."
       ]
     }
   }
