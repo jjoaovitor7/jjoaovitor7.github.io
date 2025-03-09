ca70305eb0a8e707073a1ebd88accf8011e2968b
João Vítor <48962164+jjoaovitor7@users.noreply.github.com>
2025-03-08 21:31:11 -0300

    feat: refatoração do NativeTemplate e atualização / ajuste de textos.

--- public/scripts/translation.js
+++ public/scripts/translation.js
@@ -6,12 +6,11 @@ const translations = {
     "projects": "Projetos",
     "presentation": "Apresentação",
     "about": [
-      "Sou formado em Ciência da Computação e tenho mais de 4 anos de experiência \
+      "Graduado em Ciência da Computação e tenho mais de 4 anos de experiência \
         no setor de TI, com foco no desenvolvimento de API, web & mobile.",
-      "Estou trabalhando como desenvolvedor full-stack na Empresa Sergipana de Tecnologia de Informação (EMGETIS), onde \
-        desenvolvo novas funcionalidades para sites de órgãos governamentais \
-        responsável pelo desenvolvimento do app \"Gol da Gente\" (API, Android, iOS e Web), \
-        cujo objetivo é trocar NFe's por ingressos de eventos esportivos.",
+      "No momento trabalho como dev., desenvolvendo novas funcionalidades para sites de órgãos governamentais, \
+       e apps mobile como o \"Gol da Gente\" (API, Android, iOS e Web), \
+        cujo objetivo é trocar NFe's por ingressos para eventos esportivos.",
       "Além disso, também tenho familiaridade com Kanban \
       e posso auxiliar em traduções textuais de Inglês (en) para Português Brasileiro (pt-BR).",
     ],
@@ -21,16 +20,32 @@ const translations = {
       "Desenvolvedor Mobile",
       "Tradutor"
     ],
-    "education_1": {
-      "title": "Bacharelado em Ciência da Computação pela Universidade Tiradentes - UNIT.",
-      "period": "jan. 2018 - ago. 2023",
-    },
-    "work_1": {
-      "title": "Desenvolvedor Full-Stack, EMGETIS.",
-      "period": "ago. 2021 - até o momento",
-      "info": [
-        "(alocado na ARSIN)",
-        "Atuando no desenvolvimento e manutenção de mais de 32 sites WordPress com a stack \
+    "educations": [
+      {
+        "title": "Bacharelado em Ciência da Computação pela Universidade Tiradentes - UNIT.",
+        "period": "jan. 2018 - ago. 2023",
+      }
+    ],
+    "works": [
+      {
+        "title": "Estagiário de TI, EMGETIS.",
+        "period": "dez. 2019 - ago. 2021",
+        "info": [
+          "Atuei no desenvolvimento e manutenção de sites WordPress com a stack \
+            <abbr title=\"Linux, Apache2, MariaDB e PHP\">LAMP</abbr>, \
+            além do suporte à usuários em sistemas do Detran (emulador) \
+            e em softwares de e-mail \"Expresso Livre\" e \"Zimbra\", acompanhando e solucionando os chamados através do GLPI. \
+            Obtive uma visão geral do software de monitoramento \"Zabbix\" e do Ansible \
+            e realizei backup de base de dados <abbr title=\"Microsoft SQL Server\">mssql</abbr> \
+            usando o <abbr title=\"Server Management Studio\">SMSS</abbr> e acesso remoto via RDesktop."
+        ]
+      },
+      {
+        "title": "Desenvolvedor Full-Stack, EMGETIS.",
+        "period": "ago. 2021 - jan. 2025 ",
+        "info": [
+          "(alocado na ARSIN)",
+          "Atuando no desenvolvimento e manutenção de mais de 32 sites WordPress com a stack \
         <abbr title=\"Linux, Apache2, MariaDB e PHP\">LAMP</abbr>, \
         incluindo: \
           implementação do Composer, o integrando com o Bower e o NPM para gerenciar dependências dos temas, \
@@ -38,32 +53,25 @@ const translations = {
           configuração de alguns plugins (ACF, Ninja Forms, entre outros) buscando melhorar a \
           <abbr title=\"Experiência do Usuário\">UX</abbr>, \
           melhorias de segurança e webscraping.",
-        "Implementação do Git com Gitea para versionamento do código.",
-        "Desenvolvi o app mobile \"Gol da Gente\" com React Native \
+          "Implementação do Git com Gitea para versionamento do código.",
+          "Desenvolvi o app mobile \"Gol da Gente\" com React Native \
           e disponibilizei em lojas virtuais (Google Play Store e Apple App Store), \
           além disso desenvolvi a API p/ o app com JS/TS e PostgreSQL (pela sua eficiência), \
           aplicando conceitos como MVC, patterns como Middleware e Repository, \
           e a integrando com a API da SEFAZ-SE, do gov.br, do IBGE e com o webservice do ViaCEP (json), \
           e por fim realizei o <i>port</i> do app para versão web.",
-        "Desenvolvimento de aplicação com Angular, Bootstrap 5, Golang e Docker Compose.",
-        "Prototipação com o software Lunacy.",
-        "Desenvolvi o app mobile \"SAP - SE\" (Sistema de Administração Penitenciária [SAP] do Estado de Sergipe [SE]) com React Native \
+          "Desenvolvimento de aplicação com Angular, Bootstrap 5, Golang e Docker Compose.",
+          "Prototipação com o software Lunacy.",
+          "Desenvolvi o app mobile \"SAP - SE\" (Sistema de Administração Penitenciária [SAP] do Estado de Sergipe [SE]) com React Native \
           utilizando a API do SAP da SEJUC e conceitos como redux, contracts, entre outros."
-      ],
-    },
-    "work_2": {
-      "title": "Estagiário de TI, EMGETIS.",
-      "period": "dez. 2019 - ago. 2021",
-      "info": [
-        "Atuei no desenvolvimento e manutenção de sites WordPress com a stack \
-          <abbr title=\"Linux, Apache2, MariaDB e PHP\">LAMP</abbr>, \
-          além do suporte à usuários em sistemas do Detran (emulador) \
-          e em softwares de e-mail \"Expresso Livre\" e \"Zimbra\", acompanhando e solucionando os chamados através do GLPI. \
-          Obtive uma visão geral do software de monitoramento \"Zabbix\" e do Ansible \
-          e realizei backup de base de dados <abbr title=\"Microsoft SQL Server\">mssql</abbr> \
-          usando o <abbr title=\"Server Management Studio\">SMSS</abbr> e acesso remoto via RDesktop."
-      ]
-    }
+        ]
+      },
+      {
+        "title": "Analista de Sistemas, ZDoc.",
+        "period": "jan. 2025 - até o momento",
+        "info": []
+      }
+    ],
   },
   "en": {
     "index": "Home",
@@ -74,9 +82,9 @@ const translations = {
     "about": [
       "I'm graduated in Computer Science and I have more than 4 years of experience \
         in IT sector, mainly in API, web & mobile development.",
-      "I'm working as full-stack developer at EMGETIS, where I \
-        develop new features for public authority websites and \
-        responsible for the development of the app \"Gol da Gente\" (API, Android, iOS and Web), \
+      "I currently work as developer, \
+        developing new features for public authority websites, \
+       and mobile applications such as \"Gol da Gente\" (API, Android, iOS and Web), \
         whose objective is to exchange NF-e's for sporting events tickets.",
       "Additionally, I'm also familiar with Kanban\
         and can help with textual translations from English (en) to Brazilian Portuguese (pt-BR)."
@@ -87,48 +95,57 @@ const translations = {
       "Mobile Developer",
       "Translator"
     ],
-    "education_1": {
-      "title": "Bachelor's degree in Computer Science from Tiradentes University - UNIT",
-      "period": "jan. 2018 - aug. 2023",
-    },
-    "work_1": {
-      "title": "Full-Stack Developer, EMGETIS.",
-      "period": "aug. 2021 - until now",
-      "info": [
-        "(allocated at ARSIN)",
-        "Working on the development and maintenance of over 32 WordPress sites using the \
+    "educations": [
+      {
+        "title": "Bachelor's degree in Computer Science from Tiradentes University - UNIT",
+        "period": "jan. 2018 - aug. 2023",
+      }
+    ],
+    "works": [
+      {
+        "title": "IT Intern, EMGETIS.",
+        "period": "dec. 2019 - aug. 2021",
+        "info": [
+          "I worked on the development and maintenance of WordPress sites using the \
+          <abbr title=\"Linux, Apache2, MariaDB, and PHP\">LAMP</abbr> stack, \
+          as well as providing support to users of Detran systems (emulator) \
+          and email software \"Expresso Livre\" and \"Zimbra\", tracking and resolving tickets through GLPI. \
+          I obtained an overview of the monitoring software \"Zabbix\" \
+          and Ansible and also performed <abbr title=\"Microsoft SQL Server\">mssql</abbr> database backups \
+          using <abbr title=\"Server Management Studio\">SMSS</abbr> and remote access via RDesktop."
+        ]
+      },
+      {
+        "title": "Full-Stack Developer, EMGETIS.",
+        "period": "aug. 2021 - jan. 2025",
+        "info": [
+          "(allocated at ARSIN)",
+          "Working on the development and maintenance of over 32 WordPress sites using the \
           <abbr title=\"Linux, Apache2, MariaDB and PHP\">LAMP</abbr> stack, \
           including: \
             implementing Composer and integrating it with Bower and NPM to manage theme dependencies, \
             creating shortcodes, custom blocks and \
             configuring plugins (ACF, Ninja Forms, among others) to improve the <abbr title=\"User Experience\">UX</abbr>, \
             security improvements, and web scraping.",
-        "Git implementation with Gitea for code versioning.",
-        "I developed the \"Gol da Gente\" mobile app using React Native \
+          "Git implementation with Gitea for code versioning.",
+          "I developed the \"Gol da Gente\" mobile app using React Native \
           and I deployed in virtual stores (Google Play Store and Apple App Store), besides I developed \
           the API with JS/TS and PostgreSQL (through its efficience), \
           applying concepts such as MVC, patterns as Middleware and Repository, \
           integrating it with the SEFAZ-SE API, gov.br, IBGE and the ViaCEP (json), \
           and I perfomed the <i>port</i> of app to the web version.",
-        "Development of application with Angular, Bootstrap 5, Golang and Docker Compose.",
-        "Prototyping with Lunacy software.",
-        "I developed the mobile app \"SAP - SE\" (Penitentiary Administration System of Sergipe State) with React Native \
+          "Development of application with Angular, Bootstrap 5, Golang and Docker Compose.",
+          "Prototyping with Lunacy software.",
+          "I developed the mobile app \"SAP - SE\" (Penitentiary Administration System of Sergipe State) with React Native \
           using SEJUC SAP API and concepts as redux, contracts, among others."
-      ],
-    },
-    "work_2": {
-      "title": "IT Intern, EMGETIS.",
-      "period": "dec. 2019 - aug. 2021",
-      "info": [
-        "I worked on the development and maintenance of WordPress sites using the \
-          <abbr title=\"Linux, Apache2, MariaDB, and PHP\">LAMP</abbr> stack, \
-          as well as providing support to users of Detran systems (emulator) \
-          and email software \"Expresso Livre\" and \"Zimbra\", tracking and resolving tickets through GLPI. \
-        I obtained an overview of the monitoring software \"Zabbix\" \
-          and Ansible and also performed <abbr title=\"Microsoft SQL Server\">mssql</abbr> database backups \
-          using <abbr title=\"Server Management Studio\">SMSS</abbr> and remote access via RDesktop."
-      ]
-    }
+        ],
+      },
+      {
+        "title": "Systems Analyst, ZDoc.",
+        "period": "jan. 2025 - until now",
+        "info": []
+      }
+    ]
   }
 }
 
@@ -139,39 +156,39 @@ if (!["en", "pt-br"].includes(userLang)) {
 }
 document.documentElement.lang = userLang;
 
+const extractIndex = (str) => {
+  const match = str.match(/\[([^\]]+)\]/);
+  return match ? parseInt(match[1]) : null;
+};
+
+const removeIndex = (str) => str.replace(/\[.*?\]/g, '');
+
 function NativeTemplate() {
   const htmlContent = document.body.innerHTML;
+
   const fmtContent = htmlContent.replace(/\{\{(.*?)\}\}/g, (_, key) => {
     key = key.trim();
-    const valueArr = key.split('.');
-    let arrOption;
+    const parts = key.split('.');
+    let extractedIndex;
 
-    const getOption = (str) => str.match(/\[([^\]]+)\]/);
-    const hasOptions = (arr) => ![null, undefined].includes(arr);
-    const clearOption = (str) => str.replace(/\[.*?\]/g, '');
+    let translationRef = translations[userLang];
 
-    if (valueArr.length == 2) {
-      arrOption = getOption(valueArr[1]);
-      if (hasOptions(arrOption)) {
-        valueArr[1] = clearOption(valueArr[1])
-        return translations[userLang][valueArr[1]][parseInt(arrOption[1])];
+    for (let i = 1; i < parts.length; i++) {
+      extractedIndex = extractIndex(parts[i]);
+      let _key = removeIndex(parts[i]);
+
+      if (typeof translationRef === "object" && _key in translationRef) {
+        translationRef = translationRef[_key];
+      }
+
+      if (extractedIndex !== null && Array.isArray(translationRef)) {
+        translationRef = translationRef[extractedIndex];
       }
     }
 
-    if (valueArr.length == 3) {
-      arrOption = getOption(valueArr[2]);
-      if (hasOptions(arrOption)) {
-        valueArr[2] = clearOption(valueArr[2])
-        return translations[userLang][valueArr[1]][valueArr[2]][parseInt(arrOption[1])];
-      }
-    }
-
-    if (valueArr.length <= 2) {
-      return translations[userLang][valueArr[1]];
-    } else {
-      return translations[userLang][valueArr[1]][valueArr[2]];
-    }
+    return translationRef;
   });
+
   document.body.innerHTML = fmtContent;
 }
 
